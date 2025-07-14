aa3eba5f3afcdc0b48748f2d15aa12f4cab41a55
Sumit-ops357 <sumitrathod22724@gmail.com>
2025-07-15 00:08:20 +0530

    Use REACT_APP_API_URL for API base URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; // Adjust the URL as needed
+const API_URL = process.env.REACT_APP_API_URL; // Adjust the URL as needed
 
 // User authentication
 export const registerUser = async (userData) => {
